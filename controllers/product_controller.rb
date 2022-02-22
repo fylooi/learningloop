425dfb588a00f1ec008f80e0296a19501767dc5f
Looi <fy.looi@gmail.com>
2022-02-23 01:04:49 +0800

    set up boilerplate to show products

--- controllers/product_controller.rb
+++ controllers/product_controller.rb
@@ -2,7 +2,20 @@ require_relative 'base_controller'
 
 class ProductController < BaseController
   def index
-    @products = ["Product 1", "Product 2", "Product 3"]
+    @products = [
+      OpenStruct.new(
+        name: 'Fancy Product',
+        description: 'This is a really fancy product',
+        price: '$100.00',
+        image_url: "https://placebeard.it/640x360"
+      ),
+      OpenStruct.new(
+        name: 'Not so fancy product',
+        description: 'Less fancy product for less money',
+        price: '$50.00',
+        image_url: "https://placebeard.it/640x360"
+      )
+    ]
   end
 
   def show
