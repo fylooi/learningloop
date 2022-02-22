0b673e144a25b12a1c8ddee6e1fb4d42ffa0fd07
Looi <fy.looi@gmail.com>
2022-02-23 01:18:31 +0800

    sketchiest checkout flow ever

--- controllers/product_controller.rb
+++ controllers/product_controller.rb
@@ -7,13 +7,15 @@ class ProductController < BaseController
         name: 'Fancy Product',
         description: 'This is a really fancy product',
         price: '$100.00',
-        image_url: "https://placebeard.it/640x360"
+        image_url: "https://placebeard.it/640x360",
+        payment_link: 'https://buy.stripe.com/test_fZe16waAlcqD21GdQR'
       ),
       OpenStruct.new(
         name: 'Not so fancy product',
         description: 'Less fancy product for less money',
         price: '$50.00',
-        image_url: "https://placebeard.it/640x360"
+        image_url: "https://placebeard.it/640x360",
+        payment_link: 'https://buy.stripe.com/test_6oEg1qfUFfCP49O7su'
       )
     ]
   end
