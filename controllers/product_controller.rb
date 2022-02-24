1d3e34a873351a009ae9cfefe725fe78dddfac97
Looi <fy.looi@gmail.com>
2022-02-25 01:26:12 +0800

    set up boilerplate actions to redirect to Stripe

--- controllers/product_controller.rb
+++ controllers/product_controller.rb
@@ -6,20 +6,23 @@ class ProductController < BaseController
       OpenStruct.new(
         name: 'Fancy Product',
         description: 'This is a really fancy product',
-        price: '$100.00',
+        price: 100.00,
+        currency: '$',
         image_url: "https://placebeard.it/640x360",
-        payment_link: 'https://buy.stripe.com/test_fZe16waAlcqD21GdQR'
-      ),
-      OpenStruct.new(
-        name: 'Not so fancy product',
-        description: 'Less fancy product for less money',
-        price: '$50.00',
-        image_url: "https://placebeard.it/640x360",
-        payment_link: 'https://buy.stripe.com/test_6oEg1qfUFfCP49O7su'
+        id: 1
+        ),
+        OpenStruct.new(
+          name: 'Not so fancy product',
+          description: 'Less fancy product for less money',
+          price: 50.00,
+          currency: '$',
+          image_url: "https://placebeard.it/640x360",
+          id: 2
       )
     ]
   end
 
-  def show
+  def checkout
+    redirect_to '/'
   end
 end
