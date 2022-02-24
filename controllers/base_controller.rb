d4ec536f89f355b1ff377e055abac509a60d2fd3
Looi <fy.looi@gmail.com>
2022-02-25 01:05:11 +0800

    implement redirect_to

--- controllers/base_controller.rb
+++ controllers/base_controller.rb
@@ -7,13 +7,23 @@ class BaseController
     # variables are loaded in the controller action
     send(action)
 
-    [200, {'Content-Type' => 'text/html'}, [render_view(action)]]
+    if @redirect_path
+      [303, {'Location' => @redirect_path}, []]
+    else
+      [200, {'Content-Type' => 'text/html'}, [render_template(default_template_path(action))]]
+    end
   end
 
-  def render_view(action)
-    dir = self.class.to_s.split("Controller").first.downcase
-    template_path = File.expand_path(File.dirname(__FILE__) + "/../views/#{dir}/#{action}.slim")
-    
+  def redirect_to(path)
+    @redirect_path = path
+  end
+
+  def render_template(template_path)
     Slim::Template.new(template_path).render(self)
+  end
+  
+  def default_template_path(action)
+    dir = self.class.to_s.split("Controller").first.downcase
+    template_path = File.expand_path(File.dirname(__FILE__) + "/../views/#{dir}/#{action}.slim")  
   end
 end
